2e24818322d7f6e109fd76778f1dfc7bd38ff9bd
Yaqing <yaqingj@udel.edu>
2025-12-05 13:04:34 -0500

    change 4

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 #define WEEK 			DAY*7
 #define YEAR 			DAY*365
 //Configurable
-#define NUMASTEROIDS 100
+#define NUMASTEROIDS 1000
 #define GRAV_CONSTANT 6.67e-11 //the gravitational constant
 #define MAX_DISTANCE 5000.0
 #define MAX_VELOCITY 50000.0
